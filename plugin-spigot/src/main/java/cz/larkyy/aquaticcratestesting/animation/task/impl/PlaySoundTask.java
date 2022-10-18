9538b700ddacf648974b4b5886f82cb4486aed43
MrLarkyy_ <studiotadikl@gmail.com>
2022-10-18 16:15:07 +0200

    - Added Offset argument to PlaySoundTask

--- plugin-spigot/src/main/java/cz/larkyy/aquaticcratestesting/animation/task/impl/PlaySoundTask.java
+++ plugin-spigot/src/main/java/cz/larkyy/aquaticcratestesting/animation/task/impl/PlaySoundTask.java
@@ -3,7 +3,9 @@ package cz.larkyy.aquaticcratestesting.animation.task.impl;
 import cz.larkyy.aquaticcratestesting.animation.Animation;
 import cz.larkyy.aquaticcratestesting.animation.task.Task;
 import cz.larkyy.aquaticcratestesting.animation.task.TaskArgument;
+import org.bukkit.Location;
 import org.bukkit.Sound;
+import org.bukkit.util.Vector;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +19,7 @@ public class PlaySoundTask extends Task {
         ARGUMENTS.add(new TaskArgument("sound","minecraft:block.note_block.bell",true));
         ARGUMENTS.add(new TaskArgument("pitch",1d,false));
         ARGUMENTS.add(new TaskArgument("volume",100f,false));
+        ARGUMENTS.add(new TaskArgument("offset","0;0;0",false));
     }
 
     public PlaySoundTask(Map<String, Object> arguments) {
@@ -25,11 +28,30 @@ public class PlaySoundTask extends Task {
 
     @Override
     public void run(Animation animation) {
+        Location location;
+        if (animation.getModel() == null) {
+            location = animation.getPlayer().getLocation();
+        } else {
+            location = animation.getModel().getLocation().clone().add(readVector(getArg("offset").toString()));
+        }
+
         animation.getPlayer().playSound(
-                animation.getPlayer().getLocation(),
+                location,
                 getArg("sound").toString().toLowerCase(),
                 Float.parseFloat(getArg("volume").toString()),
                 Float.parseFloat(getArg("pitch").toString())
+        );
+    }
+
+    private Vector readVector(String str) {
+        String[] strs = str.split(";");
+        if (strs.length < 3) {
+            return new Vector();
+        }
+        return new Vector(
+                Double.parseDouble(strs[0]),
+                Double.parseDouble(strs[1]),
+                Double.parseDouble(strs[2])
         );
     }
 }
