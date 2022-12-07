dfdf2c35725cb4775f019667184a5b7aa3bd78ac
MrLarkyy_ <48769171+MrLarkyy@users.noreply.github.com>
2022-12-07 17:04:20 +0100

    Removed another debug... sorry

--- plugin-spigot/src/main/java/cz/larkyy/aquaticcratestesting/crate/CrateHandler.java
+++ plugin-spigot/src/main/java/cz/larkyy/aquaticcratestesting/crate/CrateHandler.java
@@ -113,7 +113,6 @@ public class CrateHandler {
     public boolean isInAnimation(Player p) {
         for (Crate c : crates.values()) {
             if (c.getAnimationManager().get().isInAnimation(p)) {
-                Bukkit.broadcastMessage("Player is in an animation of crate: "+c.getIdentifier());
                 return true;
             }
         }
