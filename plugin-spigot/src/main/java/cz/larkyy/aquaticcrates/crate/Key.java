372961b9ce408319aa8feeed18807073d63bab67
MrLarkyy_ <studiotadikl@gmail.com>
2024-11-26 00:22:09 +0100

    Enhance 'give' method to update item metadata with PAPI placeholders
    Refactor the 'give' method to update item metadata, including display name and lore, with PAPI placeholders before giving the items to players. This ensures the items reflect dynamic player-specific information, improving customization.

--- plugin-spigot/src/main/java/cz/larkyy/aquaticcrates/crate/Key.java
+++ plugin-spigot/src/main/java/cz/larkyy/aquaticcrates/crate/Key.java
@@ -2,20 +2,26 @@ package cz.larkyy.aquaticcrates.crate;
 
 import cz.larkyy.aquaticcrates.AquaticCrates;
 import cz.larkyy.aquaticcrates.api.AquaticCratesAPI;
+import gg.aquatic.aquaticseries.lib.AquaticSeriesLib;
+import gg.aquatic.aquaticseries.lib.StringExtKt;
 import gg.aquatic.aquaticseries.lib.adapt.AquaticString;
 import gg.aquatic.aquaticseries.lib.item.CustomItem;
+import gg.aquatic.aquaticseries.lib.util.CollectionExtKt;
+import gg.aquatic.aquaticseries.spigot.adapt.SpigotString;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Key {
 
     private static final NamespacedKey KEY
-            = new NamespacedKey(AquaticCrates.getPlugin(AquaticCrates.class),"KeyIdentifier");
+            = new NamespacedKey(AquaticCrates.getPlugin(AquaticCrates.class), "KeyIdentifier");
 
     private final CustomItem item;
     private final Crate crate;
@@ -45,10 +51,23 @@ public class Key {
     public void give(List<Player> players, int amount) {
         ItemStack is = getItem(amount);
 
-        players.forEach(p->{
+        players.forEach(p -> {
+            var im = is.getItemMeta();
+
+            var displayName = StringExtKt.updatePAPIPlaceholders(Objects.requireNonNullElse(AquaticSeriesLib.Companion.getINSTANCE().getAdapter().getItemStackAdapter().getAquaticDisplayName(im), StringExtKt.toAquatic("")), p);
+            var previouslore = AquaticSeriesLib.Companion.getINSTANCE().getAdapter().getItemStackAdapter().getAquaticLore(im);
+            var newLore = new ArrayList<AquaticString>();
+            for (AquaticString aquaticString : previouslore) {
+                newLore.add(StringExtKt.updatePAPIPlaceholders(aquaticString,p));
+            }
+
+            AquaticSeriesLib.Companion.getINSTANCE().getAdapter().getItemStackAdapter().displayName(displayName,im);
+            AquaticSeriesLib.Companion.getINSTANCE().getAdapter().getItemStackAdapter().lore(newLore,im);
+
+            is.setItemMeta(im);
             var map = p.getInventory().addItem(is.clone());
-            map.forEach((i,item) -> {
-                p.getLocation().getWorld().dropItem(p.getLocation(),item);
+            map.forEach((i, item) -> {
+                p.getLocation().getWorld().dropItem(p.getLocation(), item);
             });
         });
     }
@@ -66,7 +85,7 @@ public class Key {
         is.setAmount(amount);
 
         ItemMeta im = is.getItemMeta();
-        im.getPersistentDataContainer().set(KEY, PersistentDataType.STRING,crate.getIdentifier());
+        im.getPersistentDataContainer().set(KEY, PersistentDataType.STRING, crate.getIdentifier());
         is.setItemMeta(im);
 
         return is;
@@ -81,7 +100,7 @@ public class Key {
         ItemMeta im = is.getItemMeta();
         if (im == null) return null;
 
-        String id = im.getPersistentDataContainer().get(KEY,PersistentDataType.STRING);
+        String id = im.getPersistentDataContainer().get(KEY, PersistentDataType.STRING);
         if (id == null) return null;
         else return get(id);
     }
